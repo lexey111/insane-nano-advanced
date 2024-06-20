4f14f9bac9a297850ed6988d14a6fe1b1ccf831d
Oleksii Koshkin <lexey111@gmail.com>
2024-06-20 15:47:15 +0200

    fix build scripts

--- dist/js/dashboard-2HHLUYUV.js
+++
@@ -1,6 +0,0 @@
-new EventSource("/esbuild").addEventListener("change", () => setTimeout(() => location.reload(), 1000));
-"use strict";
-(() => {
-  // src/js/dashboard.js
-  console.log("Dashboard init");
-})();
